9f1379b11e3b06314f4fce24d740d9e1affc095a
fschneider <fs@agidev.fr>
2017-10-06 15:37:14 +0200

    add pylaas_core dependency into setup.py

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['arrow', 'pylaas-core'],
+    install_requires=['arrow', 'pylaas_core'],
 
     dependency_links=[
         "git+https://github.com/Agi-dev/pylaas_core.git#egg=pylaas_core"
